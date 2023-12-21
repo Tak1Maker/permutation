62abdf01543ef260d54679d4debc800c83c65872
Salim Mouttaki <MOU@inokscapital.com>
2023-12-21 16:52:55 +0100

    added stuff

--- Permutation/Program.cs
+++ Permutation/Program.cs
@@ -1,6 +1,8 @@
 ﻿//Given 2 strings, write a method to decide if one is a permutation of the other.
 // Explanation, if 2 words have the same letters in same quantity, its an permutation bcs its an anagram.
-bool IsPerumted(string word1, string word2)
+using System.Collections.Generic;
+
+bool IsPerumtedQuadratique(string word1, string word2)
 {
     var word1List = CountNumbersOfLetters(word1);
 
@@ -29,6 +31,66 @@ bool IsPerumted(string word1, string word2)
     }
 }
 
+List<int> Tri(List<int> numbers)
+{
+
+    for(int i = 0; i < numbers.Count - 1; i++)
+    {
+        var temp = 0; 
+
+        if(numbers[i] > numbers[i + 1])
+        {
+            temp = numbers[i + 1];
+            numbers[i + 1] = numbers[i];
+            numbers[i] = temp;
+        }
+    }
+    return numbers;
+}
+
+List<int> numbers = new List<int>() { 1, 20, 12, 17, 55, 9 };
+
+numbers.ForEach(x => Console.Write($"{x}-"));
+
+var n = Tri(numbers);
+Console.WriteLine();
+n.ForEach(x => Console.Write($"{x}-"));
+
+void IsPermutedLogarithmique(string word1, string word2)
+{
+    var alphabet = new Dictionary<int, string>();
+
+    for (char c = 'A'; c <= 'Z'; c++)
+    {
+        int key = c - 'A' + 1;
+        alphabet.Add(key, c.ToString().ToLower());
+    }
+
+    var word1Alphabet = "";
+
+    for(int i = 1; i < alphabet.Count; i++)
+    {
+        for(int j = 0; j < word1.Length; j++)
+        {
+            if (alphabet[i] == word1[j].ToString())
+                word1Alphabet += word1[j];
+        }
+    }
+    Console.WriteLine(word1Alphabet);
+}
+
+IsPermutedLogarithmique("tttsavb", "word2");
+
+static string SortString(string input)
+{
+    char[] chars = input.ToCharArray();
+
+    Array.Sort(chars);
+
+    return new string(chars);
+}
+
+
 List<Word> CountNumbersOfLetters(string word)
 {
     var numbers = new List<Word>();
@@ -50,7 +112,7 @@ List<Word> CountNumbersOfLetters(string word)
     return numbers;
 }
 
-Console.WriteLine(IsPerumted("eell", "llee"));
+//Console.WriteLine(IsPermutedLogarithmique("couu", "ccou"));
 public class Word
 {
     public string Letter { get; set; }
